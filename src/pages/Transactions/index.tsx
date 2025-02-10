e435c4b9249b21edff938217601c2b3d1b611a11
Vinicius Oliveira <viniciuz6460@gmail.com>
2025-02-09 21:19:10 -0300

    transactions table component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
     return(
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/01/2025</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Lanche</td>
+                            <td>
+                                <PriceHighlight variant="outcome">R$ 49,00</PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>25/01/2025</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
